fc095722a9347407c4a7f0452c7ab0c05aa07404
rodolfoHOk <rodolfo.okino@gmail.com>
2022-05-24 22:40:58 -0300

    add basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { NgxBootstrapIconsModule, list } from 'ngx-bootstrap-icons';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -9,9 +13,26 @@ import { MenuComponent } from './menu/menu.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ScrapsComponent } from './scraps/scraps.component';
 
+const icons = {
+  list,
+};
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, MenuComponent, ProfileComponent, ScrapsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    ProfileComponent,
+    ScrapsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    NgxBootstrapIconsModule.pick(icons),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
